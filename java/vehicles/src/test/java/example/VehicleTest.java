46193af1b7d0ada8c9389e763a4f474113b0411d
lukasze <wyspianski.lukasz@gmail.com>
2022-04-26 22:20:15 +0200

    update VehicleTest

--- java/vehicles/src/test/java/example/VehicleTest.java
+++ java/vehicles/src/test/java/example/VehicleTest.java
@@ -10,7 +10,7 @@ class VehicleTest {
     @Test
     void testVehicleList() {
         /*
-            List <Vehicle> vehicles = we can add only objects of type Vehicle (implementing the Vehicle interface).
+            List <Vehicle> vehicles => we can add only objects of type Vehicle (only classes that implement the Vehicle interface).
 
             Check the code below - a compiler error
             List<Vehicle> vehiclesWithApple = List.of(new Car(), new Apple());
@@ -21,20 +21,20 @@ class VehicleTest {
 
         //  ref type|ref name| list of Vehicles (car, boat, plane)
         for (Vehicle vehicle: vehicles) {
-            // The reference type - which methods can we call - one method only, no matter the objects' class
+            // The reference type - which methods can we call. Vehicle type gives us one method only, no matter the actual objects' class
             vehicle.startEngine();
             // Let's check the actual type of an object
             if(vehicle instanceof Car) {
 /*
                 ((Car) vehicle).isTheLightOn();
 
-                Line 19 is an equivalent of line 23, 24
+                Line 29 is an equivalent of line 39, 40
  */
                 /*
-                       If an object's type is a car, we can call car specific methods.
+                       If an object's type is Car, we can call Car's specific methods.
                         Since the reference type gives us available methods,
                         we need to have another reference pointing to the same object (of type Car),
-                        but this time the reference's type will give us access to all Car's meth
+                        but this time the reference's type will give us access to all Car's methods
                  */
                 Car car = (Car) vehicle;
                 car.isTheLightOn();
